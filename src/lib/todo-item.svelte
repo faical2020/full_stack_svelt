d68b1e4a66bd7215de3260085a7dd33ef6379358
faical2020 <faical.sali@gmail.com>
2022-02-10 13:38:25 +0100

    seconde2

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,14 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="" value="" />
+        <button aria-label="mark done/not done">Done/notDone</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text" />
+        <button aria-label="save todo">Save</button>
+    </form>
+    <form action="" method="">
+        <button aria-label="delete todo">Delete</button>
+    </form>
+</div>
